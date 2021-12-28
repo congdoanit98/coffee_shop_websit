3ab423ab3a342e3f35f73c5095f6a5b62862ac18
Cong Doan <congdoanit98@gmail.com>
2021-12-28 11:45:46 +0700

    code - menu slide

--- main.js
+++ main.js
@@ -1,50 +1,29 @@
-// var number_picture = 4;
+// var tl = gsap.timeline();
 
-// function previous_picture() {
-//     var current_src = document.querySelector("#slider-img").getAttribute("src");
-//     var index_img_string = current_src.substring(current_src.indexOf("_") + 1, current_src.indexOf("_") + 2);
-//     var index_img_int = Number.parseInt(index_img_string);
+// tl.from(".list-item", {duration: 1, y: 100, opacity: 0, ease: "power3"})
+// 
+// gsap.registerPlugin(ScrollTrigger);
+        
+// const project = gsap.from(".story-content", {
+//     delay: 0.25,
+//     y: 50,
+//     duration: 2,
+//     opacity: 0,
+//     ease: "power3",
+//     stagger: 0.5
+// });
 
-//     if (index_img_int == 1){
-//         index_img_int = 6;
-//     }
-//     else{
-//         --index_img_int;
-//     }
-
-//     var path_img_target = "./assets/img/slider/slider_" + index_img_int.toString() + ".jpg";
-
-//     document.querySelector("#slider-img").setAttribute("src",path_img_target);
-    
-// }
-
-// function next_picture() {
-//     var current_src = document.querySelector("#slider-img").getAttribute("src");
-//     var index_img_string = current_src.substring(current_src.indexOf("_") + 1, current_src.indexOf("_") + 2);
-//     var index_img_int = Number.parseInt(index_img_string);
-
-//     if (index_img_int == 6){
-//         index_img_int = 1;
-//     }
-//     else{
-//         ++index_img_int;
-//     }
-
-//     var path_img_target = "./assets/img/slider/slider_" + index_img_int.toString() + ".jpg";
-
-//     document.querySelector("#slider-img").setAttribute("src",path_img_target);
-// }
- 
-// setInterval(next_picture,4000);
-
-
-var imgSlider = document.querySelector('#lastClone');
-var allImgs = document.querySelectorAll('.image-contain-slider img');
-// var haaha = document.querySelector('.image-contain-slider');
-
-// alert(imgSlider)
-
-// let counter = 1;
-// const size = allImgs[0].clientWidth;
-
-// imgSlider.style.transform = 'translateX(' + -size * counter + 'px)';
+// ScrollTrigger.create({
+//     trigger: ".story-content",
+//     animation: project,
+//     // Uncomment these to see how they affect the ScrollTrigger
+//     markers: true,
+//     start: "top 500px",
+//     // end: "top 100px",
+//     // toggleClass: "active",
+//     // pin: true,
+//     // scrub: 1,
+//     // onUpdate: self => {
+//     //   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+//     // }
+// });
